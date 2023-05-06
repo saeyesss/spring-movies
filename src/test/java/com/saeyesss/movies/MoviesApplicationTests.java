2746b84da3f56e5e9205154eae85b29e9752dad5
Shreyas Nidugala <94211639+saeyesss@users.noreply.github.com>
2023-05-06 12:29:29 +0530

    init

---
+++ src/test/java/com/saeyesss/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saeyesss.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
